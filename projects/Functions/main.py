f4fb4ce6785e5745806601caa30582fd7158d8d6
alwayswong <68604011+alwayswong@users.noreply.github.com>
2021-03-01 19:37:08 -1000

    drills

---
+++ projects/Functions/main.py
@@ -0,0 +1,43 @@
+# customize the function with an input
+# parameter is the name of the data/variable (name in this case)
+# argument is the actual value of the parameter (jacob in this case)
+#
+# def greet(name):
+#     print(f'hello {name}')
+#     print('hello')
+#     print('hello')
+#
+# greet('jacob')
+#
+# # be more explicit with keyword arguemnts
+# def greet_with(name,location):
+#     print(f'hello {name}')
+#     print(f'What is it like in {location}?')
+#
+# greet_with(name='jacob',location='hawaii')
+#
+# import math
+#
+# def paint(height,width):
+#     coverage = 5
+#     cans = math.ceil((height * width) / coverage)
+#     print(f"You need {cans} cans of paint")
+#
+# paint(2,4)
+
+def prime_check(number):
+    prime = 'Y'
+    for i in range(2,number-1):
+        if number%i == 0:
+            prime = 'N'
+            break
+    print(prime)
+
+
+
+n = int(input("Check this number:"))
+prime_check(n)
+
+
+
+
