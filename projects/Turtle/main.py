f05d116f4f9d879d0f6d201efa48a9416074bd20
alwayswong <68604011+alwayswong@users.noreply.github.com>
2021-03-10 19:46:18 -1000

    little update

--- projects/Turtle/main.py
+++ projects/Turtle/main.py
@@ -1,14 +1,36 @@
-from turtle import Turtle, Screen
+import turtle
+import random
 
-timmy = Turtle()
+timmy = turtle.Turtle()
 timmy.shape('turtle')
 timmy.color('red')
 
-for i in range(20):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
+colors = ['lavender','medium spring green','orange']
+turtle.colormode(255)
+direction = [0,90,180,270]
+turtle.speed('fastest')
+# for i in range(20):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
+
+# draw a bunch of shapes within each other
+# for i in range(3,11):
+#     timmy.color(random.choice(colors))
+#     for j in range(i):
+#         angle = 360 / i
+#         timmy.right(angle)
+#         timmy.forward(100)
+
+while True:
+    timmy.pensize(10)
+    timmy.color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+    timmy.right(random.choice(direction))
+    timmy.forward(20)
+
+# random walk
+
 
 screen = Screen()
 
