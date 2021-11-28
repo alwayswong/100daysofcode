808aa86be59835431075299831b38d3b7402abc9
alwayswong <68604011+alwayswong@users.noreply.github.com>
2021-11-28 00:27:28 -0800

    adding thicker lines

--- projects/Generative Art/Matrica2/generate_art.py
+++ projects/Generative Art/Matrica2/generate_art.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFilter
 import random
 
 def generate_art():
@@ -7,6 +7,7 @@ def generate_art():
     print("Building")
     image_color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
     image_size_x = 2000
+    padding_size = 200
     image_size_y = 2000
 
     image = Image.new(
@@ -15,14 +16,14 @@ def generate_art():
         color=image_color)
     # Draw some lines on top of the background
     draw = ImageDraw.Draw(image)
-    line_count = random.randint(1,10)
+    line_count = random.randint(1,100)
     points = []
 
     # generate points
     for i in range(line_count):
         point_1 = (
-            random.randint(0,image_size_x),
-            random.randint(0,image_size_y)
+            random.randint(padding_size,image_size_x - padding_size),
+            random.randint(padding_size,image_size_y - padding_size)
         )
         #point_2 = (random.randint(0,image_size_x), random.randint(0,image_size_y))
         points.append(point_1)
@@ -39,10 +40,10 @@ def generate_art():
 
         line_xy = (p1,p2)
         line_color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-        draw.line(line_xy,fill=line_color,width=5)
 
+        draw.line(line_xy,fill=line_color,width=random.randint(1,100))
 
-
+    #image.filter(ImageFilter.SMOOTH_MORE)
     image.save(f'/Users/jacobwong/PycharmProjects/100daysofcode/projects/Generative Art/Matrica2/arts/test_i.png')
 
 if __name__ == "__main__":
